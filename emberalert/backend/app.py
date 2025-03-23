cab13a93c705c056dba7731c58a6185431abd2cd
jugpan <pjugraj@yahoo.com>
2025-03-22 22:52:46 -0700

    Created app.py
    Started the backend with Flask. Main function just uses Python to get wildfire data and then converts it into JSON format.

---
+++ emberalert/backend/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import pandas as pd
+
+app = Flask(__name__)
+CORS(app)
+
+MAP_KEY = 'b7f1b1174e56b9b7b17cbcd413b062af'
+
+@app.route('/api/wildfires')
+def get_wildfire_data():
+	try:
+		area_url = 'https://firms.modaps.eosdis.nasa.gov/api/area/csv/' + MAP_KEY + '/VIIRS_NOAA20_NRT/world/2'
+		df = pd.read_csv(area_url)
+		df = df[['latitude', 'longitude', 'acq_date', 'acq_time', 'bright_ti4', 'bright_ti5']]
+		data = df.to_dict(orient='records')
+		return jsonify(data)
+
+	except Exception as e:
+		return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+	app.run(debug=True) 
+		
+     
